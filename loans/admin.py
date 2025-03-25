da78c2e8c6e5d581f683bd1bfef9c817f9964065
poya <poya_kob@live.com>
2025-03-25 16:58:20 +0330

    loanrequest and loanpayment models added to loan app

--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import LoanRequest, LoanPayment
 
-# Register your models here.
+
+@admin.register(LoanRequest)
+class LoanRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "user", "amount", "request_date_jalali", "is_approved", "approval_date_jalali", "monthly_installment")
+    list_filter = ("is_approved", "request_date")
+
+
+@admin.register(LoanPayment)
+class LoanPaymentAdmin(admin.ModelAdmin):
+    list_display = ("loan", "amount", "payment_date_jalali", "is_approved")
+    list_filter = ("is_approved", "payment_date")
--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,38 @@
+import jdatetime
 from django.db import models
+from django.utils.timezone import localtime
+from users.models import User
 
-# Create your models here.
+
+class LoanRequest(models.Model):
+    """مدل مربوط به درخواست وام توسط کاربران"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()  # مبلغ وام
+    request_date = models.DateTimeField(auto_now_add=True)  # تاریخ درخواست
+    is_approved = models.BooleanField(default=False)  # وضعیت تأیید ادمین
+    approval_date = models.DateTimeField(null=True, blank=True)  # تاریخ تأیید وام
+    repayment_months = models.PositiveIntegerField(default=20)  # تعداد ماه‌های بازپرداخت (ثابت: 20 ماه)
+    monthly_installment = models.PositiveIntegerField(null=True, blank=True)  # مبلغ هر قسط
+
+    def calculate_installment(self):
+        """محاسبه مبلغ هر قسط (تقسیم مبلغ وام بر 20 ماه)"""
+        self.monthly_installment = self.amount // 20
+
+    def save(self, *args, **kwargs):
+        self.calculate_installment()
+        super().save(*args, **kwargs)
+
+
+class LoanPayment(models.Model):
+    """مدل مربوط به پرداخت اقساط وام"""
+    loan = models.ForeignKey(LoanRequest, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()  # مبلغ پرداخت‌شده
+    payment_date = models.DateTimeField(auto_now_add=True)  # تاریخ پرداخت
+    receipt_image = models.ImageField(upload_to="loan_receipts/", null=True, blank=True)  # تصویر فیش پرداختی
+    is_approved = models.BooleanField(default=False)  # تأیید پرداخت توسط ادمین
+
+    def payment_date_jalali(self):
+        return jdatetime.datetime.fromgregorian(datetime=localtime(self.payment_date)).strftime("%Y/%m/%d - %H:%M")
+
+    def __str__(self):
+        return f"{self.loan.user.username} - قسط {self.amount} تومان ({self.payment_date_jalali()})"
